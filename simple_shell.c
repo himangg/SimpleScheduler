d5752a08b9fcc089ede0eaa78499169cd4ca15b1
Himang Garg <130616649+himang4321@users.noreply.github.com>
2023-10-12 19:12:13 +0530

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -101,7 +101,7 @@ for (i = 0; i < strlen(command); i++) {
         }
         
     } if(status>0) {
-        add_process(pid,priority,command_name);
+        add_process(pid,priority,command);
         // at this i need to start the timer for the waititng time for the process with pid=pid
         wait(NULL); // Wait for the child process to complete
         // at this point the child process has finished, so remove the process 
